f87efff83658b41a7935ab96a09c0ca17616e7dd
kovacsalexandru77 <72850124+kovacsalexandru77@users.noreply.github.com>
2025-04-25 10:59:33 +0200

    Add files via upload

---
+++ SANDUPLUGIN/MoviesDrive/build.gradle.kts
@@ -0,0 +1,18 @@
+version = "19"
+
+cloudstream {
+    language = "hu"
+    description = "Movies and TV Shows"
+    authors = listOf("megix")
+
+    status = 1
+
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "AsianDrama",
+        "Anime"
+    )
+
+    iconUrl = "https://github.com/SaurabhKaperwan/CSX/raw/refs/heads/master/MoviesDrive/icon.png"
+}
